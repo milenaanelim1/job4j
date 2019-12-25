5c6d5802bd6672da104f2d433c2be6894adb0800
milenaanelim1 <milena.furmer2004@gmail.com>
2019-12-25 18:35:15 +0300

    Max

---
+++ chapter_001/src/main/java/job4j/condition/Max.java
@@ -0,0 +1,8 @@
+package job4j.condition;
+
+public class Max {
+    public static int max(int left, int right) {
+        int result = left>right ? left : right;
+        return result;
+    }
+}
---
+++ chapter_001/src/test/java/job4j/condition/MaxTest.java
@@ -0,0 +1,13 @@
+package job4j.condition;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class MaxTest {
+    @Test
+    public void whenMax1To2Then2() {
+        int result = Max.max(2, 1);
+        assertThat(result, is(2));
+    }
+}
