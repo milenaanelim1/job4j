63c043f169995b4aa167be55735832878619385e
milenaanelim1 <milena.furmer2004@gmail.com>
2019-12-26 13:23:32 +0300

    CounterLoop

---
+++ chapter_001/src/test/java/job4j/loop/CounterTest.java
@@ -0,0 +1,14 @@
+package job4j.loop;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class CounterTest {
+    @Test
+    public void whenSumEvenNumbersFromOneToTenThenThirty() {
+        int sum = Counter.add(1,10);
+        assertThat(sum, is(30));
+    }
+}
