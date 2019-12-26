893831d53d5991d5fc096b8108335f2dbc3502da
milenaanelim1 <milena.furmer2004@gmail.com>
2019-12-26 14:49:41 +0300

    FitnessLoop

---
+++ chapter_001/src/main/java/job4j/loop/Fitness.java
@@ -0,0 +1,14 @@
+package job4j.loop;
+
+public class Fitness {
+    public int calc(int ivan, int nik) {
+        int month = 0;
+        while (ivan<nik){
+            ivan = ivan*3;
+            nik = nik*2;
+            month = month+1;
+        }
+
+        return month;
+    }
+}
