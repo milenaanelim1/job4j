da4d8194e580f2b59ecaf5689d99770a63e4991f
milenaanelim1 <milena.furmer2004@gmail.com>
2019-12-29 20:07:09 +0300

    FactorialLoop

--- chapter_001/src/main/java/job4j/loop/Factorial.java
+++ chapter_001/src/main/java/job4j/loop/Factorial.java
@@ -5,8 +5,6 @@ public class Factorial {
         int result = 1;
         for (int a = 1; a <= n; a++) {
             result= a*result;}
-        if(n==0){
-        }
         return result;
         }
 }
