c99f72c751b2f98876e8a22dac6d89a17691199e
milenaanelim1 <milena.furmer2004@gmail.com>
2020-01-13 22:14:19 +0300

    CalculatorJavaDoc

--- chapter_001/src/main/java/job4j/calculator/Calcuator.java
+++ chapter_001/src/main/java/job4j/calculator/Calcuator.java
@@ -1,25 +1,58 @@
 package job4j.calculator;
 
+/**
+ * Class Calculator для вычисления арифметических операций + - * :
+ * @author elena
+ * @since 13.01.2020
+ */
+
 public class Calcuator {
+
+    /**
+     * Сложение
+     * @param first первый аргумент
+     * @param second второй аргумент
+     */
     public static void add(int first, int second) {
         int result = first + second;
         System.out.println(first +"+"+second+"="+result);
     }
 
+    /**
+     * Умножение
+     * @param first первый аргумент
+     * @param second второй аргумент
+     */
     public static void multiply(int first, int second) {
         int result = first * second;
         System.out.println(first +"*"+second+"="+result);
     }
 
+    /**
+     * Деление
+     * @param first первый аргумент
+     * @param second второй аргумент
+     */
     public static void div(int first, int second) {
         int result = first / second;
         System.out.println(first +"/"+second+"="+result);
     }
 
+    /**
+     * Вычитание
+     * @param first первый аргумент
+     * @param second второй аргумент
+     */
     public static void subtrack(int first, int second) {
         int result = first - second;
         System.out.println(first +"-"+second+"="+result);
     }
+
+
+    /**
+     * Main.
+     * @param args -args.
+     */
     public static void main(String[] args){
         add(1,1);
         div(4,2);
