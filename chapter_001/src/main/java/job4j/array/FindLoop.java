80331af60c80d0224ea9239374f060ebdf4868c3
milenaanelim1 <milena.furmer2004@gmail.com>
2020-01-19 17:14:41 +0300

    FindLoopArray

--- chapter_001/src/main/java/job4j/array/FindLoop.java
+++ chapter_001/src/main/java/job4j/array/FindLoop.java
@@ -1,26 +1,28 @@
 package job4j.array;
 
 public class FindLoop {
-    /**   public int indexOf(int[] data, int el) {
-        int rst = -1; // если элемента нет в массиве, то возвращаем -1.
-        for (int index = 0; index != data.length; index++) {
-            if (data[index] == el) {
-                rst = index;
-                break;
-            }
-            return rst;
-        }
-      */
+    /**
+     * public int indexOf(int[] data, int el) {
+     * int rst = -1; // если элемента нет в массиве, то возвращаем -1.
+     * for (int index = 0; index != data.length; index++) {
+     * if (data[index] == el) {
+     * rst = index;
+     * break;
+     * }
+     * return rst;
+     * }
+     */
 
-    public static int indexOf2 (int[] data2, int el2, int start, int finish){
+    public static int indexOf2(int[] data2, int el2, int start, int finish) {
         int rst2 = -1; // если элемента нет в массиве, то возвращаем -1.
 
-        for (int index = start+1; index <= finish; index++) {
-            if (data2[index] == el2){
+        for (int index = start; index <= finish; index++) {
+            if (data2[index] == el2) {
                 rst2 = index;
                 break;
             }
         }
-        return rst2; }
+        return rst2;
+    }
 }
 //}
