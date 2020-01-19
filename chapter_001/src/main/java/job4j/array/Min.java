489c5d110d61e7ca5eb72cad91e07f951d4ca3e5
milenaanelim1 <milena.furmer2004@gmail.com>
2020-01-19 17:52:25 +0300

    MinArray

---
+++ chapter_001/src/main/java/job4j/array/Min.java
@@ -0,0 +1,18 @@
+package job4j.array;
+
+public class Min {
+    public static int findMin(int[] array) {
+        int min = array[0];    // 10 2 5 6
+        // 0 1 2 3
+        for (int index = 0; index < array.length; index++) {
+
+            if (min > array[index]) {
+                min = array[index];
+
+            }
+
+        }
+        return min;
+
+    }
+}
