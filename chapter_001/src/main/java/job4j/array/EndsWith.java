0f486f93d690a59ed6b6833460135457d1c8f4e2
milenaanelim1 <milena.furmer2004@gmail.com>
2020-01-19 17:08:16 +0300

    EndsWithArray

--- chapter_001/src/main/java/job4j/array/EndsWith.java
+++ chapter_001/src/main/java/job4j/array/EndsWith.java
@@ -2,19 +2,20 @@ package job4j.array;
 
 public class EndsWith {
     public static boolean endsWith(char[] word, char[] post) {
-        boolean result = false;
-        for(int index = 1; index != post.length; index++ ){
+        boolean result = true;
+        for (int index = 1; index != post.length; index++) {
 
-            if(word[word.length- index] == post[post.length- index]) {
-            result = true;
+            // if(word[word.length- index] == post[post.length- index]) {
+            // result = true;
 
-            }
-            else if(word[word.length- index] != post[post.length- index]){
+
+            if (word[word.length - index] != post[post.length - index]) {
                 result = false;
                 break;
             }
-
         }
+
+
         return result;
-}
+    }
 }
