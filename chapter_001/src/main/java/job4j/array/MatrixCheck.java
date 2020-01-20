79c0ea3b8d5fe08104166743fa98d15ed1418a95
milenaanelim1 <milena.furmer2004@gmail.com>
2020-01-20 23:14:42 +0300

    MatrixCheckArray

---
+++ chapter_001/src/main/java/job4j/array/MatrixCheck.java
@@ -0,0 +1,23 @@
+package job4j.array;
+
+public class MatrixCheck {
+    public static boolean isWin(char[][] board) {
+        boolean result = false;
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                if (i == j) {
+                    if (board[i][j] == 'X') {
+                        for (int index = 0; index != 5; index++) {
+                            if ((board[i][j] == board[i + 1][j]) || (board[i][j] == board[i][j + 1])) ;
+                            result = true;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        return result;
+    }
+}
+
+
