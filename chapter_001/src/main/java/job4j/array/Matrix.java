664787879f968da6df84d395fabbe311ba844eeb
milenaanelim1 <milena.furmer2004@gmail.com>
2020-01-20 22:00:24 +0300

    MatrixArray

---
+++ chapter_001/src/main/java/job4j/array/Matrix.java
@@ -0,0 +1,24 @@
+package job4j.array;
+
+public class Matrix {
+
+    public int[][] multiple(int size) {
+        int[][] table = new int[size][size];
+
+        int i = 0;
+        int j = 0;
+        table[i][j] = 1;
+        for (i = 0; i < size; i++) {
+            for (j = 0; j < size; j++) {
+
+                    table[i][j] = (i) * (j);
+
+
+            }
+        }
+
+
+        return table;
+    }
+
+}
