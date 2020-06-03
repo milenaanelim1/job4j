662c2be65ef54efa64cb369c4dbd0e449a4e39fd
milena <milena.furmer2004@gmail.com>
2020-06-03 16:19:22 +0300

    turnArray

--- chapter_001/src/main/java/job4j/array/Turn.java
+++ chapter_001/src/main/java/job4j/array/Turn.java
@@ -1,16 +1,12 @@
 package job4j.array;
 
 public class Turn {
-    public int[] back(int[] array) {
-        for (int index = 0; index != (array.length)/2; index++) {
-//4 1 6 2
-            //if (array.length%2==0){
-                int theme = array[index];
-                array[index]=array[array.length-1-index];
-                array[array.length-1-index] = theme;
-           // }
-
-        }
-        return array;
-    }
+  public int[] back(int[]array) {
+      for(int i = 0; i != (array.length)/2; i++){
+          int theme = array[i];
+          array[i]=array[array.length-1-i];
+          array[array.length-1-i]=theme;
+      }
+      return array;
+  }
 }
