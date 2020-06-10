73f76125248c95a85f11f04f24c1eb3c4f3bb635
milena <milena.furmer2004@gmail.com>
2020-06-10 16:37:14 +0300

    1.7.Состояние объекта[#203982]

---
+++ chapter_002/src/main/java/ru/job4j/oop/Battery.java
@@ -0,0 +1,23 @@
+package ru.job4j.oop;
+
+import java.net.InetAddress;
+
+public class Battery {
+    private int load;
+
+    public Battery(int charge){
+        this.load = charge;
+    }
+    public void exchange(Battery another){
+        another.load = another.load + this.load;
+        this.load = 0;
+    }
+
+    public static void main (String[]args){
+        Battery first = new Battery(80);
+        Battery second = new Battery(20);
+        System.out.println("first : " + first.load + ". second : " + second.load);
+        first.exchange(second);
+        System.out.println("first : " + first.load + ". second : " + second.load);
+    }
+}
