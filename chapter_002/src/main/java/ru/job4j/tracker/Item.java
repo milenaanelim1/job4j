21d82e7737e01006b4ca0284375ffeb8af57f4dd
milena <milena.furmer2004@gmail.com>
2020-06-13 16:17:42 +0300

    2. Перегрузка конструктора. [#203989]

---
+++ chapter_002/src/main/java/ru/job4j/tracker/Item.java
@@ -0,0 +1,16 @@
+package ru.job4j.tracker;
+
+public class Item {
+private String id;
+private String name;
+
+public Item(){
+}
+public Item(String id){
+    this.id = id;
+}
+public Item(String id, String name){
+    this.id = id;
+    this.name = name;
+}
+}
