d9ff3ed56c21d63e14053245db7a4fd6f19f481f
milena <milena.furmer2004@gmail.com>
2020-06-15 13:20:42 +0300

    4. Переопределение [#203990]

---
+++ chapter_002/src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -0,0 +1,13 @@
+package ru.job4j.inheritance;
+
+public class JSONReport extends TextReport {
+    public String generate(String name, String body){
+        return "name : "+ name + System.lineSeparator() + "body : " + body;
+    }
+    public static void main(String[]args){
+        JSONReport report = new JSONReport();
+        String text = report.generate("bvz","ntkj");
+        System.out.println(text);
+    }
+
+}
