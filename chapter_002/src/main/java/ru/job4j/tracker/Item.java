780b208d26583fccf7cefc6eaf2d90fccb23b25a
milena <milena.furmer2004@gmail.com>
2020-06-22 17:44:55 +0300

    ...

--- chapter_002/src/main/java/ru/job4j/tracker/Item.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Item.java
@@ -1,16 +1,22 @@
 package ru.job4j.tracker;
 
 public class Item {
-private String id;
-private String name;
+    private int id;
+    private String name;
 
-public Item(){
-}
-public Item(String id){
-    this.id = id;
-}
-public Item(String id, String name){
-    this.id = id;
-    this.name = name;
-}
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,4 +1,61 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
+    private final Item[] items = new Item[100];
+    private int ids = 1;
+    private int size = 0;
+
+    public Item add(Item item) {
+        item.setId(ids++);
+        items[size++] = item;
+        return item;
+    }
+
+    public Item[] findAll(Item[] items, int size){
+        this.size = size;
+        Item[] itemsWithoutNull = new Item[items.length];
+        for (int index = 0; index < items.length; index++) {
+            Item item = items[index];
+            if (item != null) {
+                itemsWithoutNull[size] = item;
+                size++;
+            }
+        }
+        itemsWithoutNull = Arrays.copyOf(itemsWithoutNull, size);
+        for (int index = 0; index < itemsWithoutNull.length; index++) {
+            System.out.println(itemsWithoutNull[index]);
+        }
+       return items;
+    }
+
+    public Item[] findByName(String key){
+        Item[] itemsWithoutNull = new Item[items.length];
+        int sizw = 0;
+        for(int i = 0; i<items.length;i++){
+            Item item = items[i];
+            if(item.getName().equals(key)){
+                itemsWithoutNull[sizw] = item;
+                sizw++;
+            }
+        }
+        itemsWithoutNull = Arrays.copyOf(itemsWithoutNull, sizw);
+        for (int index = 0; index < itemsWithoutNull.length; index++) {
+            System.out.println(itemsWithoutNull[index]);
+        }
+        return items;
+    }
+
+    public Item findById(int id) {
+        Item rsl = null;
+        for (int index = 0; index < size; index++) {
+            Item item = items[index];
+            if (item.getId() == id) {
+                rsl = item;
+                break;
+            }
+        }
+        return rsl;
+    }
 }
