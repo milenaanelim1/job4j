58f44d4598b0dae0808828b115ad20f6deadcb90
milena <milena.furmer2004@gmail.com>
2020-06-24 20:39:45 +0300

    3. Интерфейс Input [#203953]

---
+++ chapter_002/src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,18 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport{
+    @Override
+    public void ride() {
+
+    }
+
+    @Override
+    public void passengers(int number) {
+
+    }
+
+    @Override
+    public int price(int fuel) {
+        return 0;
+    }
+}
