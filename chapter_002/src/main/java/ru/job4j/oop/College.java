71fa1243e23a9b43416be4395f7391c7cfb95e07
milena <milena.furmer2004@gmail.com>
2020-06-27 11:36:23 +0300

    3.1. Приведение типов. Повышение типа и понижение типа. [#203956]

---
+++ chapter_002/src/main/java/ru/job4j/oop/College.java
@@ -0,0 +1,10 @@
+package ru.job4j.oop;
+
+public class College {
+    public static void main(String[]args){
+        Freshman freshman =new Freshman();
+        Student st = freshman;
+        Object obj = freshman;
+        //Повышающее приведение(up casting)
+    }
+}
